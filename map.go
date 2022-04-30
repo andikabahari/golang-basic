f4ceb39eed5df6bb8e1d233cc7c4a47edca0b915
Andika Bahari <kzlfxx@gmail.com>
2022-04-30 23:52:17 +0700

    map

--- map.go
+++ map.go
@@ -11,4 +11,13 @@ func main() {
 	fmt.Println(person)
 	fmt.Println(person["firstName"])
 	fmt.Println(person["lastName"])
+
+	song := make(map[string]string)
+	song["title"] = "Giant Steps"
+	song["composer"] = "John Coltrane"
+	song["any"] = "..."
+	fmt.Println(song)
+
+	delete(song, "any")
+	fmt.Println(song)
 }
